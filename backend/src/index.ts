8facfe9145d0d2f01daa605383d80f3845b9ac23
Manoj13579 <duwaikoho.4hai@gmail.com>
2025-04-26 20:06:01 +0545

    changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,7 @@ import session from 'express-session';
 import passport from 'passport';
 import { stripeWebhooks } from './controllers/webhooks';
 import { app, server } from './socketio/socketio';
+import MongoStore from "connect-mongo";
 /* app imported above coz You want your app to have only one Express instance.Shared app = consistency.If you did this instead in index.ts:const app = express(); Now you'd have two different app instances: One in socketio.ts (used for Socket.IO’s server same server here).One in index.ts (used for routes/middleware). That would lead to middleware or routes being registered on one app, while the server is using the other. Nothing would work as expected! 
 You should always use a single Express instance (app) per project/server. In very rare, advanced microservice-like architectures, someone might create multiple Express apps in the same codebase. For example:
 One app for admin routes. One app for public API routes*/
@@ -16,24 +17,27 @@ One app for admin routes. One app for public API routes*/
 
 
 
-// app.use(session({
-//     secret: process.env.SESSION_SECRET_KEY!,
-//     resave: false,
-//     saveUninitialized: false,
-//     store: MongoStore.create({
-//       mongoUrl: process.env.MONGO_URI,
-//       collectionName: "sessions",
-//        // remove the session document from the mongodb when cookie expires
-//        autoRemove: "native",
-//     }),
-//     cookie: {
-//       secure: process.env.NODE_ENV === "production",
-//       httpOnly: true,
-//       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-//       path: '/',
-//       maxAge: 24 * 60 * 60 * 1000,
-//     },
-//   }));
+app.set("trust proxy", 1);
+
+
+app.use(session({
+    secret: process.env.SESSION_SECRET_KEY!,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      collectionName: "sessions",
+       // remove the session document from the mongodb when cookie expires
+       autoRemove: "native",
+    }),
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      path: '/',
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  }));
   
 
 
